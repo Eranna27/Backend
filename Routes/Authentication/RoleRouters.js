9845a0d24c23f3182b4780b627eefda674ad6e2d
madugundueranna <meranna60@gmail.com>
2026-01-31 23:40:40 +0530

    Backend is live

--- Routes/Authentication/RoleRouters.js
+++ Routes/Authentication/RoleRouters.js
@@ -3,6 +3,9 @@ const roleRouters = new express.Router();
 const roleController = require("../../Controllers/Authentication/RoleController");
 
 // Login
+router.get("/", (req, res) => {
+    res.send("Backend is live 🚀");
+});
 
 roleRouters.post("/login", roleController.login);
 roleRouters.post("/register", roleController.register);
