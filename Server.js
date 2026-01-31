6a11a3c541dac4212e5db0f17bda029545161e8d
madugundueranna <meranna60@gmail.com>
2026-01-31 23:15:39 +0530

    some changes

--- Server.js
+++ Server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const multer = require("multer");
 require("./Config/DBConnect");
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const router = require("./Routes/Routers");
 
 app.use(cors());
