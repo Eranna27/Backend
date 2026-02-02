0701ddec4ab3199a026fb48a737f1f46e2a56bfd
madugundueranna <meranna60@gmail.com>
2026-02-02 15:23:24 +0530

    some changes in google sing

--- Server.js
+++ Server.js
@@ -5,12 +5,13 @@ require("./Config/DBConnect");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const router = require("./Routes/Routers");
+const passport = require("./Controllers/Authentication/GoogleAuthController");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(router);
-
+app.use(passport.initialize());
 
 // Server Starting
 
